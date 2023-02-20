21c2bd259d1df52f51371d178895d1b510441cd2
Robert Lukasonok <70327485+roluk@users.noreply.github.com>
2023-02-20 11:19:24 +0200

    Reformat files

--- packages/changed-elements-react/src/widgets/ChangedElementsWidget.tsx
+++ packages/changed-elements-react/src/widgets/ChangedElementsWidget.tsx
@@ -11,7 +11,7 @@ import { IModelApp, IModelConnection, ScreenViewport } from "@itwin/core-fronten
 import { ScrollPositionMaintainer } from "@itwin/core-react";
 import { SvgAdd, SvgCompare, SvgStop } from "@itwin/itwinui-icons-react";
 import { IconButton, ProgressRadial } from "@itwin/itwinui-react";
-import * as React from "react";
+import { Component, createRef, ReactElement } from "react";
 
 import { ChangeElementType, type ChangedElementEntry } from "../api/ChangedElementEntryCache";
 import { VersionCompareUtils, VersionCompareVerboseMessages } from "../api/VerboseMessages";
@@ -26,15 +26,14 @@ import {
 } from "./EnhancedElementsInspector";
 import { openSelectDialog } from "./VersionCompareSelectWidget";
 
-export const changedElementsWidgetAttachToViewportEvent = new BeEvent<
-  (vp: ScreenViewport) => void
->();
+export const changedElementsWidgetAttachToViewportEvent = new BeEvent<(vp: ScreenViewport) => void>();
 
-/** Props for changed elements widget */
+/** Props for changed elements widget. */
 export interface ChangedElementsWidgetProps {
-  /** Optional manager if you don't want the default static VersionCompare.manager to be used */
+  /** Optional manager if you don't want the default static VersionCompare.manager to be used. */
   manager?: VersionCompareManager;
-  /** Used to maintain scroll positions in widget controls */
+
+  /** Used to maintain scroll positions in widget controls. */
   rootElementRef?: React.Ref<HTMLDivElement>;
 }
 
@@ -52,18 +51,13 @@ export interface ChangedElementsWidgetState {
 }
 
 /**
- * Widget to display changed elements and inspect them further
- * This widget contains functionality to hide/show type of change
- * Filter based on properties, inspect models, save visualization filters
+ * Widget to display changed elements and inspect them further. This widget contains functionality to hide/show type of
+ * change. Filter based on properties, inspect models, save visualization filters.
  */
-export class ChangedElementsWidget extends React.Component<
-  ChangedElementsWidgetProps,
-  ChangedElementsWidgetState
-> {
-  public static get widgetId() {
-    return "ChangedElementsWidget";
-  }
-  private _onComparisonStarting = () => {
+export class ChangedElementsWidget extends Component<ChangedElementsWidgetProps, ChangedElementsWidgetState> {
+  public static readonly widgetId = "ChangedElementsWidget";
+
+  private _onComparisonStarting = (): void => {
     this.setState({
       loading: true,
       loaded: false,
@@ -72,7 +66,7 @@ export class ChangedElementsWidget extends React.Component<
     });
   };
 
-  private _onComparisonStopped = () => {
+  private _onComparisonStopped = (): void => {
     this.setState({
       message: IModelApp.localization.getLocalizedString("VersionCompare:versionCompare.comparisonNotActive"),
       description: IModelApp.localization.getLocalizedString("VersionCompare:versionCompare.comparisonGetStarted"),
@@ -85,7 +79,7 @@ export class ChangedElementsWidget extends React.Component<
     currentIModel: IModelConnection,
     targetIModel: IModelConnection,
     elements: ChangedElementEntry[],
-  ) => {
+  ): void => {
     this.setState({
       message: "",
       description: "",
@@ -97,11 +91,8 @@ export class ChangedElementsWidget extends React.Component<
     });
   };
 
-  private _onProgressEvent = (message: string) => {
-    this.setState({
-      message,
-      description: "",
-    });
+  private _onProgressEvent = (message: string): void => {
+    this.setState({ message, description: "" });
   };
 
   private _refreshCheckboxesEvent = new BeEvent<() => void>();
@@ -149,14 +140,14 @@ export class ChangedElementsWidget extends React.Component<
     };
   }
 
-  public override componentWillUnmount() {
+  public override componentWillUnmount(): void {
     this.state.manager.versionCompareStarting.removeListener(this._onComparisonStarting);
     this.state.manager.versionCompareStarted.removeListener(this._onComparisonStarted);
     this.state.manager.loadingProgressEvent.removeListener(this._onProgressEvent);
     this.state.manager.versionCompareStopped.removeListener(this._onComparisonStopped);
   }
 
-  private getChangedElementsContent() {
+  private getChangedElementsContent(): ReactElement {
     if (!this.state.currentIModel || !this.state.targetIModel) {
       Logger.logError(
         VersionCompare.logCategory,
@@ -175,63 +166,46 @@ export class ChangedElementsWidget extends React.Component<
     );
   }
 
-  private getLoadingContent() {
+  private getLoadingContent(): ReactElement {
     return (
       <CenteredDiv data-testid="clw-loading-content">
-        {this.state.loading && (
+        {
+          this.state.loading &&
           <>
             <ProgressRadial indeterminate size="large" />
             <span className="comparison-legend-message">
               {this.state.message}
             </span>
           </>
-        )}
-        {!this.state.loading && (
-          <EmptyStateComponent
-            icon="icon-compare"
-            title={this.state.message}
-            description={this.state.description}
-          />
-        )}
+        }
+        {
+          !this.state.loading &&
+          <EmptyStateComponent icon="icon-compare" title={this.state.message} description={this.state.description} />
+        }
       </CenteredDiv>
     );
   }
 
-  private _showAll = async () => {
-    const set =
-      this.state.manager.changedElementsManager.entryCache.getIdsOfAllChangedElements();
-    const visualizationManager =
-      this.state.manager.visualization?.getSingleViewVisualizationManager();
-    if (visualizationManager !== undefined) {
-      await visualizationManager.toggleElementsVisibility(
-        true,
-        set,
-        false,
-        false,
-      );
-    }
+  private _showAll = async (): Promise<void> => {
+    const set = this.state.manager.changedElementsManager.entryCache.getIdsOfAllChangedElements();
+    const visualizationManager = this.state.manager.visualization?.getSingleViewVisualizationManager();
+    await visualizationManager?.toggleElementsVisibility(true, set, false, false);
+
     this._refreshCheckboxesEvent.raiseEvent();
     VersionCompareUtils.outputVerbose(VersionCompareVerboseMessages.comparisonLegendWidgetShowAllSuccessful);
   };
 
-  private _hideAll = async () => {
+  private _hideAll = async (): Promise<void> => {
     const set = this.state.manager.changedElementsManager.entryCache.getIdsOfAllChangedElements();
     const visualizationManager = this.state.manager.visualization?.getSingleViewVisualizationManager();
-    if (visualizationManager !== undefined) {
-      await visualizationManager.toggleElementsVisibility(
-        false,
-        set,
-        true,
-        true,
-      );
-    }
+    await visualizationManager?.toggleElementsVisibility(false, set, true, true);
+
     this._refreshCheckboxesEvent.raiseEvent();
     VersionCompareUtils.outputVerbose(VersionCompareVerboseMessages.comparisonLegendWidgetHideAllSuccessful);
   };
 
-  private _invert = async () => {
-    const visualizationManager =
-      this.state.manager.visualization?.getSingleViewVisualizationManager();
+  private _invert = async (): Promise<void> => {
+    const visualizationManager = this.state.manager.visualization?.getSingleViewVisualizationManager();
     if (visualizationManager) {
       // Get all element Ids that are in "comparison"
       const set = this.state.manager.changedElementsManager.entryCache.getIdsOfAllChangedElements();
@@ -247,31 +221,26 @@ export class ChangedElementsWidget extends React.Component<
     }
   };
 
-  /** Go into property comparison */
-  private _handleInspect = async () => {
+  /** Go into property comparison. */
+  private _handleInspect = async (): Promise<void> => {
     this.state.manager.featureTracking.trackInspectElementTool();
     await this.state.manager.initializePropertyComparison();
     VersionCompareUtils.outputVerbose(VersionCompareVerboseMessages.comparisonLegendWidgetInitializeInspect);
   };
 
-  private _handleCompare = async () => {
+  private _handleCompare = async (): Promise<void> => {
     const iModelConnection = UiFramework.getIModelConnection();
     if (iModelConnection) {
       await openSelectDialog(iModelConnection);
     }
   };
 
-  private _handleStopCompare = async () => {
-    VersionCompare.manager
-      ?.stopComparison()
-      .then()
-      .catch(() => {
-        /* No-op */
-      });
+  private _handleStopCompare = async (): Promise<void> => {
+    VersionCompare.manager?.stopComparison().catch(() => { });
     this.setState({ loaded: false });
   };
 
-  private getHeader() {
+  private getHeader(): ReactElement {
     return (
       <>
         <IconButton
@@ -283,7 +252,8 @@ export class ChangedElementsWidget extends React.Component<
         >
           <SvgAdd />
         </IconButton>
-        {this.state.loaded && (
+        {
+          this.state.loaded &&
           <IconButton
             size="small"
             styleType="borderless"
@@ -293,8 +263,9 @@ export class ChangedElementsWidget extends React.Component<
           >
             <SvgStop />
           </IconButton>
-        )}
-        {this.state.loaded && this.state.manager.wantNinezone && (
+        }
+        {
+          this.state.loaded && this.state.manager.wantNinezone &&
           <IconButton
             size="small"
             styleType="borderless"
@@ -304,12 +275,12 @@ export class ChangedElementsWidget extends React.Component<
           >
             <SvgCompare />
           </IconButton>
-        )}
+        }
       </>
     );
   }
 
-  public override render() {
+  public override render(): ReactElement {
     return (
       <WidgetComponent data-testid="comparison-legend-widget">
         <WidgetComponent.TitleBar>
@@ -321,9 +292,7 @@ export class ChangedElementsWidget extends React.Component<
           </WidgetComponent.TitleBar.Content>
         </WidgetComponent.TitleBar>
         <WidgetComponent.Content data-testid="comparison-legend-widget-content">
-          {this.state.loaded
-            ? this.getChangedElementsContent()
-            : this.getLoadingContent()}
+          {this.state.loaded ? this.getChangedElementsContent() : this.getLoadingContent()}
         </WidgetComponent.Content>
       </WidgetComponent>
     );
@@ -334,21 +303,15 @@ export interface ChangedElementsWidgetControlOptions {
   manager?: VersionCompareManager | undefined;
 }
 
-/** ChangedElementsWidgetControl [[WidgetControl]] */
 export class ChangedElementsWidgetControl extends WidgetControl {
-  private _activeTreeRef = React.createRef<HTMLDivElement>();
+  private _activeTreeRef = createRef<HTMLDivElement>();
   private _maintainScrollPosition?: ScrollPositionMaintainer;
 
   constructor(info: ConfigurableCreateInfo, options: ChangedElementsWidgetControlOptions) {
     super(info, options);
 
     // Pass in a ref object to let us maintain scroll position
-    this.reactNode = (
-      <ChangedElementsWidget
-        manager={options?.manager}
-        rootElementRef={this._activeTreeRef}
-      />
-    );
+    this.reactNode = <ChangedElementsWidget manager={options?.manager} rootElementRef={this._activeTreeRef} />;
   }
 
   public override saveTransientState(): void {
@@ -357,74 +320,58 @@ export class ChangedElementsWidgetControl extends WidgetControl {
     }
   }
 
-  /** Return true so that we maintain state when the widget is closed and do clean-up of scroll position maintainer */
+  /** Return true so that we maintain state when the widget is closed and do clean-up of scroll position maintainer. */
   public override restoreTransientState(): boolean {
-    if (this._maintainScrollPosition) {
-      this._maintainScrollPosition.dispose();
-      this._maintainScrollPosition = undefined;
-    }
-
+    this._maintainScrollPosition?.dispose();
+    this._maintainScrollPosition = undefined;
     return true;
   }
 }
 
-const onComparisonStarting = () => {
+const onComparisonStarting = (): void => {
   // Open/Close comparison legend
-  const activeStage = FrontstageManager.activeFrontstageDef;
-  if (activeStage) {
-    const propertyWidget = activeStage.findWidgetDef(ChangedElementsWidget.widgetId);
-    if (propertyWidget) {
-      propertyWidget.setWidgetState(WidgetState.Open);
-    }
-  }
+  FrontstageManager.activeFrontstageDef
+    ?.findWidgetDef(ChangedElementsWidget.widgetId)
+    ?.setWidgetState(WidgetState.Open);
 };
 
-const onComparisonStopped = () => {
+const onComparisonStopped = (): void => {
   EnhancedListComponent.cleanMaintainedState();
 };
 
-const onStartFailed = () => {
+const onStartFailed = (): void => {
   // Open/Close comparison legend
-  const activeStage = FrontstageManager.activeFrontstageDef;
-  if (activeStage) {
-    const propertyWidget = activeStage.findWidgetDef(ChangedElementsWidget.widgetId);
-    if (propertyWidget) {
-      propertyWidget.setWidgetState(WidgetState.Hidden);
-    }
-  }
+  FrontstageManager.activeFrontstageDef
+    ?.findWidgetDef(ChangedElementsWidget.widgetId)
+    ?.setWidgetState(WidgetState.Hidden);
 };
 
-const onFrontstageReady = (args: FrontstageReadyEventArgs) => {
+const onFrontstageReady = (args: FrontstageReadyEventArgs): void => {
   const manager = VersionCompare.manager;
   if (manager === undefined) {
     return;
   }
 
-  const frontstageIds = new Set(manager?.options.ninezoneOptions?.frontstageIds ?? []);
+  const frontstageIds = new Set(manager.options.ninezoneOptions?.frontstageIds ?? []);
   if (frontstageIds.has(args.frontstageDef.id)) {
     const widget = args.frontstageDef.findWidgetDef(ChangedElementsWidget.widgetId);
-    if (widget) {
-      widget.setWidgetState(manager.isComparing ? WidgetState.Open : WidgetState.Hidden);
-    }
+    widget?.setWidgetState(manager.isComparing ? WidgetState.Open : WidgetState.Hidden);
   }
 };
 
 /**
- * Setup events for changed elements widget to react to frontstage activated and version compare events to auto-hide/show the widget
- * @param manager Version Compare Manager
+ * Setup events for changed elements widget to react to frontstage activated and version compare events to
+ * auto-hide/show the widget.
  */
-export const bindChangedElementsWidgetEvents = (manager: VersionCompareManager) => {
+export const bindChangedElementsWidgetEvents = (manager: VersionCompareManager): void => {
   manager.versionCompareStarting.addListener(onComparisonStarting);
   manager.versionCompareStopped.addListener(onComparisonStopped);
   manager.versionCompareStartFailed.addListener(onStartFailed);
   FrontstageManager.onFrontstageReadyEvent.addListener(onFrontstageReady);
 };
 
-/**
- * Clean-up events that make the widget automatically react to frontstage activated and version compare events
- * @param manager
- */
-export const unbindChangedElementsWidgetEvents = (manager: VersionCompareManager) => {
+/** Clean-up events that make the widget automatically react to frontstage activated and version compare events. */
+export const unbindChangedElementsWidgetEvents = (manager: VersionCompareManager): void => {
   manager.versionCompareStarting.removeListener(onComparisonStarting);
   manager.versionCompareStopped.removeListener(onComparisonStopped);
   manager.versionCompareStartFailed.removeListener(onStartFailed);
@@ -434,8 +381,8 @@ export const unbindChangedElementsWidgetEvents = (manager: VersionCompareManager
   onComparisonStopped();
 };
 
-/** Returns a React element containing the ChangedElementsWidgetControl in a ui-framework Widget */
-export const getChangedElementsWidget = () => {
+/** Returns a React element containing the ChangedElementsWidgetControl in a ui-framework Widget. */
+export const getChangedElementsWidget = (): ReactElement => {
   return (
     <Widget
       id={ChangedElementsWidget.widgetId}
