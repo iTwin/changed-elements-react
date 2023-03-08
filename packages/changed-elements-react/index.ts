4e6f1fd7bdc1632186e198678461daa265e1c435
Robert Lukasonok <70327485+roluk@users.noreply.github.com>
2023-03-08 18:15:10 +0200

    Re-export all changed-elements-react package contents in root index.ts

--- packages/changed-elements-react/index.ts
+++ packages/changed-elements-react/index.ts
@@ -2,5 +2,4 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-export { VersionCompare, VersionCompareSelectDialog, type VersionCompareSelectDialogProps } from "./src/index";
-export { ChangedElementsWidget } from "./src/index";
+export * from "./src/index";
