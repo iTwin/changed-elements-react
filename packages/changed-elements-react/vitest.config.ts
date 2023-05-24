b35bf9bce9e83ba714089ff6d4a57f50a4db589b
Robert Lukasonok <70327485+roluk@users.noreply.github.com>
2023-05-24 17:28:34 +0300

    Update packages

--- packages/changed-elements-react/vitest.config.ts
+++ packages/changed-elements-react/vitest.config.ts
@@ -4,7 +4,7 @@
 *--------------------------------------------------------------------------------------------*/
 import { defineConfig } from "vitest/config";
 
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
