21462e08aeeda4de27eb11c9283bdb70e1f3051f
Robert Lukasonok <70327485+roluk@users.noreply.github.com>
2023-07-04 16:24:46 +0300

    Remove AppUI from ElementsList

--- packages/changed-elements-react/src/widgets/ElementsList.tsx
+++ packages/changed-elements-react/src/widgets/ElementsList.tsx
@@ -2,11 +2,10 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { useTransientState } from "@itwin/appui-react";
 import type { TreeNodeItem } from "@itwin/components-react";
 import { BeEvent } from "@itwin/core-bentley";
 import { Presentation, type SelectionChangeEventArgs } from "@itwin/presentation-frontend";
-import { useCallback, useEffect, useRef, type ReactElement } from "react";
+import { forwardRef, useCallback, useEffect, useRef, type ReactElement } from "react";
 import { FixedSizeList } from "react-window";
 import InfiniteLoader from "react-window-infinite-loader";
 
@@ -53,176 +52,160 @@ export interface ElementsListProps {
   nodesReloaded: BeEvent<() => void>;
 }
 
-export function ElementsList(props: ElementsListProps): ReactElement {
-  const elementListRef = useRef<HTMLDivElement>(null);
-  const loaderRef = useRef<InfiniteLoader>(null);
-  const listRef = useRef<FixedSizeList>(null);
+export const ElementsList = forwardRef<HTMLDivElement, ElementsListProps>(
+  function ElementsList(props, ref): ReactElement {
+    const loaderRef = useRef<InfiniteLoader>(null);
+    const listRef = useRef<FixedSizeList>(null);
 
-  useEffect(
-    () => {
-      const handleSelectionChanged = (args: SelectionChangeEventArgs) => {
-        let ids: string[] = [];
+    useEffect(
+      () => {
+        const handleSelectionChanged = (args: SelectionChangeEventArgs) => {
+          let ids: string[] = [];
 
-        args.keys.instanceKeys.forEach((keys) => {
-          ids = [...ids, ...keys];
-        });
+          args.keys.instanceKeys.forEach((keys) => {
+            ids = [...ids, ...keys];
+          });
 
-        // Add selection set of elements that presentation added
-        const allIds = new Set([...ids, ...args.imodel.selectionSet.elements]);
+          // Add selection set of elements that presentation added
+          const allIds = new Set([...ids, ...args.imodel.selectionSet.elements]);
 
-        const findNodeToFocus = () => {
-          for (const node of props.nodes) {
-            if (allIds.has(node.id)) {
-              return node;
-            } else if (node.extendedData?.children !== undefined) {
-              const children: string[] = node.extendedData.children;
-              if (children.some(allIds.has)) {
+          const findNodeToFocus = () => {
+            for (const node of props.nodes) {
+              if (allIds.has(node.id)) {
                 return node;
+              } else if (node.extendedData?.children !== undefined) {
+                const children: string[] = node.extendedData.children;
+                if (children.some(allIds.has)) {
+                  return node;
+                }
+              }
+            }
+
+            return undefined;
+          };
+
+          if (ids.length !== 0 && listRef.current !== null) {
+            const node = findNodeToFocus();
+            if (node !== undefined && args.source !== "ChangedElementsWidget") {
+              const nodeToScroll = props.nodes.findIndex((item) => node.id === item.id);
+              if (nodeToScroll !== -1) {
+                listRef.current.scrollToItem(nodeToScroll, "smart");
               }
             }
           }
-
-          return undefined;
         };
 
-        if (ids.length !== 0 && listRef.current !== null) {
-          const node = findNodeToFocus();
-          if (node !== undefined && args.source !== "ChangedElementsWidget") {
-            const nodeToScroll = props.nodes.findIndex((item) => node.id === item.id);
-            if (nodeToScroll !== -1) {
-              listRef.current.scrollToItem(nodeToScroll, "smart");
-            }
-          }
-        }
-      };
-
-      Presentation.selection.selectionChange.addListener(handleSelectionChanged);
-      return () => {
-        Presentation.selection.selectionChange.removeListener(handleSelectionChanged);
-      };
-    },
-    [props.nodes],
-  );
-
-  useEffect(
-    () => props.nodesReloaded.addListener(() => {
-      loaderRef.current?.resetloadMoreItemsCache(true);
-    }),
-    [props.nodesReloaded],
-  );
-
-  // Workaround for AppUI bug where widget momentarily appears to have zero height
-  const scrollTopRef = useRef(0);
-  useTransientState(
-    () => {
-      const element = elementListRef.current?.firstChild as HTMLDivElement;
-      if (element) {
-        scrollTopRef.current = element.scrollTop;
-      }
-    },
-    () => {
-      const element = elementListRef.current?.firstChild as HTMLDivElement;
-      if (element) {
-        element.scrollTop = scrollTopRef.current;
-      }
-    },
-  );
-
-  const isItemLoaded = useCallback(
-    (index: number) => {
-      const entry = props.nodes[index];
-      return (0, props.isLoaded)(entry);
-    },
-    [props.nodes, props.isLoaded],
-  );
-
-  const loadMoreItems = useCallback(
-    async (startIndex: number, stopIndex: number) => {
-      const slice = props.nodes.slice(startIndex, stopIndex + 1);
-      await (0, props.loadNodes)(slice);
-    },
-    [props.nodes, props.loadNodes],
-  );
-
-
-  const renderNode = useCallback(
-    (item: TreeNodeItem) => {
-      return (
-        <ElementNodeComponent
-          id={item.id}
-          element={item.extendedData?.element}
-          selected={(0, props.isSelected)(item)}
-          label={item.label}
-          isModel={item.extendedData?.isModel ?? false}
-          opcode={item.extendedData?.element?.opcode}
-          type={item.extendedData?.element?.type ?? 0}
-          wantTypeTooltip={true}
-          hasChildren={(0, props.hasChildren)(item)}
-          loadingChildren={item.extendedData?.loadingChildren ?? false}
-          visible={(0, props.isVisible)(item)}
-          toggleVisibility={() => (0, props.toggleVisibility)(item)}
-          wantChangeSquare={true}
-          onInspect={() => (0, props.onInspect)(item)}
-          onPropertyCompare={() => (0, props.onPropertyCompare)()}
-          onClick={() => (0, props.onNodeClick)(item)}
-          indirect={item.extendedData?.element?.indirect}
-        />
-      );
-    },
-    [
-      props.isSelected,
-      props.hasChildren,
-      props.isVisible,
-      props.toggleVisibility,
-      props.onInspect,
-      props.onPropertyCompare,
-      props.onNodeClick,
-    ],
-  );
-
-  if (props.nodes.length === 0) {
-    return (
-      <div className="element-list-no-results">
-        {props.emptyMessage}
-      </div>
+        Presentation.selection.selectionChange.addListener(handleSelectionChanged);
+        return () => {
+          Presentation.selection.selectionChange.removeListener(handleSelectionChanged);
+        };
+      },
+      [props.nodes],
     );
-  }
 
-  return (
-    <AutoSizer ref={elementListRef} className="element-list">
-      {(size) => {
+    useEffect(
+      () => props.nodesReloaded.addListener(() => {
+        loaderRef.current?.resetloadMoreItemsCache(true);
+      }),
+      [props.nodesReloaded],
+    );
+
+    const isItemLoaded = useCallback(
+      (index: number) => {
+        const entry = props.nodes[index];
+        return (0, props.isLoaded)(entry);
+      },
+      [props.nodes, props.isLoaded],
+    );
+
+    const loadMoreItems = useCallback(
+      async (startIndex: number, stopIndex: number) => {
+        const slice = props.nodes.slice(startIndex, stopIndex + 1);
+        await (0, props.loadNodes)(slice);
+      },
+      [props.nodes, props.loadNodes],
+    );
+
+
+    const renderNode = useCallback(
+      (item: TreeNodeItem) => {
         return (
-          <InfiniteLoader
-            ref={loaderRef}
-            isItemLoaded={isItemLoaded}
-            itemCount={props.nodes.length}
-            loadMoreItems={loadMoreItems}
-          >
-            {({ onItemsRendered }) => {
-              return (
-                <FixedSizeList
-                  ref={listRef}
-                  style={{ overflow: "overlay" }}
-                  height={size.height}
-                  itemCount={props.nodes.length}
-                  onItemsRendered={onItemsRendered}
-                  itemSize={34}
-                  overscanCount={10}
-                  width={size.width}
-                >
-                  {(nodeProps) => {
-                    return (
-                      <div style={nodeProps.style}>
-                        {renderNode(props.nodes[nodeProps.index])}
-                      </div>
-                    );
-                  }}
-                </FixedSizeList>
-              );
-            }}
-          </InfiniteLoader>
+          <ElementNodeComponent
+            id={item.id}
+            element={item.extendedData?.element}
+            selected={(0, props.isSelected)(item)}
+            label={item.label}
+            isModel={item.extendedData?.isModel ?? false}
+            opcode={item.extendedData?.element?.opcode}
+            type={item.extendedData?.element?.type ?? 0}
+            wantTypeTooltip={true}
+            hasChildren={(0, props.hasChildren)(item)}
+            loadingChildren={item.extendedData?.loadingChildren ?? false}
+            visible={(0, props.isVisible)(item)}
+            toggleVisibility={() => (0, props.toggleVisibility)(item)}
+            wantChangeSquare={true}
+            onInspect={() => (0, props.onInspect)(item)}
+            onPropertyCompare={() => (0, props.onPropertyCompare)()}
+            onClick={() => (0, props.onNodeClick)(item)}
+            indirect={item.extendedData?.element?.indirect}
+          />
         );
-      }}
-    </AutoSizer>
-  );
-}
+      },
+      [
+        props.isSelected,
+        props.hasChildren,
+        props.isVisible,
+        props.toggleVisibility,
+        props.onInspect,
+        props.onPropertyCompare,
+        props.onNodeClick,
+      ],
+    );
+
+    if (props.nodes.length === 0) {
+      return (
+        <div className="element-list-no-results">
+          {props.emptyMessage}
+        </div>
+      );
+    }
+
+    return (
+      <AutoSizer ref={ref} className="element-list">
+        {(size) => {
+          return (
+            <InfiniteLoader
+              ref={loaderRef}
+              isItemLoaded={isItemLoaded}
+              itemCount={props.nodes.length}
+              loadMoreItems={loadMoreItems}
+            >
+              {({ onItemsRendered }) => {
+                return (
+                  <FixedSizeList
+                    ref={listRef}
+                    style={{ overflow: "overlay" }}
+                    height={size.height}
+                    itemCount={props.nodes.length}
+                    onItemsRendered={onItemsRendered}
+                    itemSize={34}
+                    overscanCount={10}
+                    width={size.width}
+                  >
+                    {(nodeProps) => {
+                      return (
+                        <div style={nodeProps.style}>
+                          {renderNode(props.nodes[nodeProps.index])}
+                        </div>
+                      );
+                    }}
+                  </FixedSizeList>
+                );
+              }}
+            </InfiniteLoader>
+          );
+        }}
+      </AutoSizer>
+    );
+  },
+);
