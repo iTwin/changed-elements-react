038a23bb2f5ec2f191ffef54aaf3b30f3d3ef533
Caleb German <Caleb.German@Bentley.com>
2024-08-02 15:25:54 -0500

    Got new models tree working. Code needs to be cleaned up

--- packages/changed-elements-react/src/widgets/comparisonJobWidget/components/ChangedElementsInspectorV2.tsx
+++ packages/changed-elements-react/src/widgets/comparisonJobWidget/components/ChangedElementsInspectorV2.tsx
@@ -1,10 +1,7 @@
 /* eslint-disable react/prop-types */
-import { isTreeModelNode, TreeModelNode } from "@itwin/components-react";
 import { Flex } from "@itwin/itwinui-react/esm";
 import { VersionCompareManager } from "../../../api/VersionCompareManager";
 import { DbOpcode, Id64String } from "@itwin/core-bentley";
-import { isPresentationTreeNodeItem } from "@itwin/presentation-components";
-import { NodeKey } from "@itwin/presentation-common";
 import { ModelsCategoryCache } from '../../../api/ModelsCategoryCache';
 import { ComponentPropsWithoutRef, useCallback, useEffect, useState } from "react";
 import { IModelConnection, Viewport } from "@itwin/core-frontend";
@@ -13,22 +10,30 @@ import { useModelsTreeButtonProps, useModelsTree, TreeWithHeader, ModelsTreeComp
 import { createStorage, SelectionStorage } from "@itwin/unified-selection";
 import { SchemaContext } from "@itwin/ecschema-metadata";
 import { ECSchemaRpcLocater } from "@itwin/ecschema-rpcinterface-common";
-// import {
-//   ClassGroupingOption, connectIModelConnection, ModelsTreeSelectionPredicate,
-//   ModelsVisibilityHandler, UiFramework, useVisibilityTreeFiltering, useVisibilityTreeRenderer,
-//   VisibilityTreeEventHandler, VisibilityTreeFilterInfo, VisibilityTreeNoFilteredData,
-// } from "@bentley/ui-framework";
 
+type CustomModelsTreeRendererProps = Parameters<ComponentPropsWithoutRef<typeof VisibilityTree>["treeRenderer"]>[0];
+type CreateNodeLabelComponentProps = Required<ComponentPropsWithoutRef<typeof VisibilityTreeRenderer>>["getLabel"];
+type PresentationHierarchyNode = Parameters<CreateNodeLabelComponentProps>[0];
+type HierarchyNode = PresentationHierarchyNode["nodeData"];
+type ColorClasses = "added" | "modified" | "";
+
+//todo should be a way to find these types from the tree widget
+type NodeType = "subject" | "model" | "category" | "element" | "class-grouping";
 
 let unifiedSelectionStorage: SelectionStorage | undefined;
 const schemaContextCache = new Map<string, SchemaContext>();
-
-type ColorClasses = "added" | "modified" | "";
+const modifiedCategoryIds = new Set<string>();
 
 type ChangedElementsInspectorV2Props = {
   manager: VersionCompareManager;
   current: IModelConnection;
   currentVP: Viewport;
+};
+
+type ElementLabelProps = {
+  color: ColorClasses;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  originalLabel: React.ReactElement<any, string | React.JSXElementConstructor<any>> | undefined;
 };
 
 // The Models tree requires a unified selection storage to support selection synchronization with the
@@ -59,71 +64,27 @@ function getSchemaContext(imodel: IModelConnection): SchemaContext {
   return schemaContext;
 }
 
-const modifiedCategoryIds = new Set<string>();
+function ElementLabel(props: ElementLabelProps) {
+  return (
+    <Flex flexDirection="row">
+      <div
+        className={`circle ${props.color}`}
+      ></div>
+      {props.originalLabel}
+    </Flex>
+  );
+}
 
-// type ElementLabelProps = TreeNodeLabelRendererProps & { color: ColorClasses; };
-// function ElementLabel(props: ElementLabelProps) {
-//   return (
-//     <Flex flexDirection="row">
-//       <div
-//         className={`circle ${props.color}`}
-//       ></div>
-//       <DefaultLabelRenderer label={props.node.label} context={props.context} />
-//     </Flex>
-//   );
-// }
+function ChangedElementsInspectorV2(v2InspectorProps: Readonly<ChangedElementsInspectorV2Props>) {
+  const buttonProps = useModelsTreeButtonProps({ imodel: v2InspectorProps.current, viewport: v2InspectorProps.currentVP });
+  const { modelsTreeProps, rendererProps } = useModelsTree({ activeView: v2InspectorProps.currentVP });
 
-// function CustomModelsTreeLabelRenderer(props: CustomModelsTreeRendererProps) {
-//   const key = extractNodeKeyFromNode(props.node);
-//   const nodeType = ModelsVisibilityHandler.getNodeType(props.node.item);
-//   const ecInstanceId = key ? key.instanceKeys[0].id : "";
-//   const [catColor, setCatColor] = useState<ColorClasses>("");
-//   const modelsCategoryData = ModelsCategoryCache.getModelsCategoryData();
-//   useEffect(() => {
-//     const findIfCategoryHasChangedElements = async () => {
-//       if (modifiedCategoryIds.has(ecInstanceId)) {
-//         setCatColor("modified");
-//         return;
-//       }
-//       for await (const row of changedElementsInspectorV2Props.current.query(
-//         `SELECT ECInstanceId as id FROM BisCore.GeometricElement3d where Category.id = ${ecInstanceId}`,
-//       )) {
-//         if (changedElementsInspectorV2Props.manager.changedElementsManager.filteredChangedElements.has(row[0])) {
-//           modifiedCategoryIds.add(ecInstanceId);
-//           setCatColor("modified");
-//           break;
-//         }
-//       }
-//     };
-//     if (nodeType === ModelsTreeNodeType.Category) {
-//       void findIfCategoryHasChangedElements();
-//     }
-//   });
-//   if (!key)
-//     return ElementLabel({ ...props, color: "" });
-//   if (changedElementsInspectorV2Props.manager.changedElementsManager.allChangeElements.has(ecInstanceId)) {
-
-//     const changeElementEntry = changedElementsInspectorV2Props.manager.changedElementsManager.allChangeElements.get(ecInstanceId);;
-//     if (changeElementEntry && nodeType === ModelsTreeNodeType.Element) {
-//       return ElementLabel({ ...props, color: getColorBasedOffDbCode(changeElementEntry.opcode) });
-//     } else if (changeElementEntry) {
-//       return ElementLabel({ ...props, color: "modified" });
-//     }
-//   }
-//   if (modelsCategoryData?.addedElementsModels.has(ecInstanceId)) {
-//     return ElementLabel({ ...props, color: "modified" });
-//   }
-//   if (nodeType === ModelsTreeNodeType.Category) {
-//     return ElementLabel({ ...props, color: catColor });
-//   }
-//   return ElementLabel({ ...props, color: "" });
-// }
-
-
-
-function ChangedElementsInspectorV2({ current, currentVP }: Readonly<ChangedElementsInspectorV2Props>) {
-  const buttonProps = useModelsTreeButtonProps({ imodel: current, viewport: currentVP });
-  const { modelsTreeProps, rendererProps } = useModelsTree({ activeView: currentVP });
+  function CustomModelsTreeRenderer(props: CustomModelsTreeRendererProps) {
+    const getLabel = useCallback<CreateNodeLabelComponentProps>(NodeLabelCreator(props, v2InspectorProps),
+      [props.getLabel],
+    );
+    return <VisibilityTreeRenderer {...props} getLabel={getLabel} />;
+  }
 
   return (
     <TreeWithHeader buttons={[<ModelsTreeComponent.ShowAllButton {...buttonProps} key={"abc123"} />, <ModelsTreeComponent.HideAllButton {...buttonProps} key={"123abc"} />]}>
@@ -131,37 +92,60 @@ function ChangedElementsInspectorV2({ current, currentVP }: Readonly<ChangedElem
         {...modelsTreeProps}
         getSchemaContext={getSchemaContext}
         selectionStorage={getUnifiedSelectionStorage()}
-        imodel={current}
+        imodel={v2InspectorProps.current}
         treeRenderer={(props) => <CustomModelsTreeRenderer {...props} {...rendererProps} />}
       />
     </TreeWithHeader>
   );
 }
 
-type CustomModelsTreeRendererProps = Parameters<ComponentPropsWithoutRef<typeof VisibilityTree>["treeRenderer"]>[0];
-type CreateNodeLabelComponentProps = Required<ComponentPropsWithoutRef<typeof VisibilityTreeRenderer>>["getLabel"];
-type PresentationHierarchyNode = Parameters<CreateNodeLabelComponentProps>[0];
-type HierarchyNode = PresentationHierarchyNode["nodeData"];
-
-//todo should be a way to find these types from the tree widget
-type NodeType = "subject" | "model" | "category" | "element" | "class-grouping";
-
-function CustomModelsTreeRenderer(props: CustomModelsTreeRendererProps) {
-  const getLabel = useCallback<CreateNodeLabelComponentProps>(NodeLabelCreator(props),
-    [props.getLabel],
-  );
-
-  return <VisibilityTreeRenderer {...props} getLabel={getLabel} />;
-}
-
-const NodeLabelCreator = (props: Pick<CustomModelsTreeRendererProps, "getLabel">) => {
+const NodeLabelCreator = (props: Pick<CustomModelsTreeRendererProps, "getLabel">, changedElementsInspectorV2Props: ChangedElementsInspectorV2Props) => {
   function CreateNodeLabelComponent(node: Readonly<PresentationHierarchyNode>) {
     const nodeType = getNodeType(node);
     const [catColor, setCatColor] = useState<ColorClasses>("");
     const modelsCategoryData = ModelsCategoryCache.getModelsCategoryData();
-    const ecInstanceId=extractEcInstanceIdFromNode(node,nodeType);
+    const ecInstanceId = extractEcInstanceIdFromNode(node, nodeType);
     const originalLabel = props.getLabel(node);
-    return <>Custom node - {originalLabel}</>;
+
+        useEffect(() => {
+          const findIfCategoryHasChangedElements = async () => {
+            if (ecInstanceId && modifiedCategoryIds.has(ecInstanceId)) {
+              setCatColor("modified");
+              return;
+            }
+            for await (const row of changedElementsInspectorV2Props.current.query(
+              `SELECT ECInstanceId as id FROM BisCore.GeometricElement3d where Category.id = ${ecInstanceId}`,
+            )) {
+              if ( ecInstanceId && changedElementsInspectorV2Props.manager.changedElementsManager.filteredChangedElements.has(row[0])) {
+                modifiedCategoryIds.add(ecInstanceId);
+                setCatColor("modified");
+                break;
+              }
+           }
+          };
+          if (nodeType === "category") {
+            void findIfCategoryHasChangedElements();
+          }
+        });
+
+
+    if (ecInstanceId === undefined) {
+      return <>{originalLabel}</>;
+    }
+    if (changedElementsInspectorV2Props.manager.changedElementsManager.allChangeElements.has(ecInstanceId)) {
+
+      const changeElementEntry = changedElementsInspectorV2Props.manager.changedElementsManager.allChangeElements.get(ecInstanceId);
+      if (changeElementEntry && (nodeType === "element" || nodeType === "subject")) {
+        return ElementLabel({ originalLabel, color: getColorBasedOffDbCode(changeElementEntry.opcode) });
+      }
+    }
+    if (modelsCategoryData?.addedElementsModels.has(ecInstanceId)) {
+      return ElementLabel({ originalLabel, color: "modified" });
+    }
+    if (nodeType === "category") {
+      return ElementLabel({ originalLabel, color: catColor });
+    }
+    return <>{originalLabel}</>;
   }
   return CreateNodeLabelComponent;
 };
@@ -184,9 +168,9 @@ const extractGroupingNodeKeyFromNode = (node: PresentationHierarchyNode) => {
   return key;
 };
 
-const extractModelEcInstanceIdFromClassGroupingNode = (node: PresentationHierarchyNode):string | undefined => {
+const extractModelEcInstanceIdFromClassGroupingNode = (node: PresentationHierarchyNode): string | undefined => {
   return node.extendedData?.modelId;
-}
+};
 
 
 const extractEcInstanceIdFromNode = (node: PresentationHierarchyNode, nodeType: NodeType) => {
@@ -195,7 +179,7 @@ const extractEcInstanceIdFromNode = (node: PresentationHierarchyNode, nodeType: 
   } else {
     return extractModelEcInstanceIdFromClassGroupingNode(node);
   }
-}
+};
 
 const getNodeType = (node: PresentationHierarchyNode): NodeType => {
   if (node.extendedData?.isSubject)
