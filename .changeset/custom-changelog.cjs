eb0fce751e8a38194479b4d9cff36494158963c0
Caleb German <Caleb.German@Bentley.com>
2025-01-15 15:43:42 -0600

    Added typing for file

--- .changeset/custom-changelog.cjs
+++ .changeset/custom-changelog.cjs
@@ -2,12 +2,22 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * Get the current package version
+ * @returns {Promise<string>} - The current package version
+ */
 async function getPackageVersion() {
   const packageJsonPath = path.resolve(__dirname, '../packages/changed-elements-react/package.json');
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
   return packageJson.version;
 }
 
+/**
+ * Increment the version based on the version type
+ * @param {string} version - The current version
+ * @param {string} versionType - The type of version increment (major, minor, patch)
+ * @returns {string} - The incremented version
+ */
 function incrementVersion(version, versionType) {
   const [major, minor, patch] = version.split('.').map(Number);
 
@@ -23,6 +33,12 @@ function incrementVersion(version, versionType) {
   }
 }
 
+/**
+ * Get the release line for the changelog
+ * @param {import('@changesets/types').NewChangesetWithCommit} changeset - The changeset object
+ * @param {string} versionType - The type of version increment (major, minor, patch)
+ * @returns {Promise<string>} - The release line for the changelog
+ */
 async function getReleaseLine(changeset, versionType) {
   const newVersion = incrementVersion(await getPackageVersion(), versionType);
   // Get the current date in yyyy-mm-dd format
@@ -32,7 +48,13 @@ async function getReleaseLine(changeset, versionType) {
   return `${releaseDateAndLinkToRelease}\n${changeset.summary}`;
 }
 
-async function getDependencyReleaseLine() {}
+/**
+ * Get the dependency release line for the changelog
+ * @returns {Promise<string>} - The dependency release line for the changelog
+ */
+async function getDependencyReleaseLine() {
+  // Implementation for dependency release line
+}
 
 const defaultChangelogFunctions = {
   getReleaseLine,
