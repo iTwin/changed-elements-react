149730e8af84f524204c3423d640429f0b7a4558
Caleb German <Caleb.German@Bentley.com>
2025-01-31 13:53:16 -0600

    Updated vitest config

--- packages/changed-elements-react/vitest.config.ts
+++ packages/changed-elements-react/vitest.config.ts
@@ -9,17 +9,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    // Workaround for https://github.com/iTwin/iTwinUI-react/issues/727
-    alias: {
-      "@itwin/itwinui-react": "@itwin/itwinui-react/esm",
-      "@itwin/appui-layout-react": "@itwin/appui-layout-react/lib/esm/appui-layout-react.js",
-      "@itwin/components-react": "@itwin/components-react/lib/esm/components-react.js",
-      "@itwin/core-react": "@itwin/core-react/lib/esm/core-react.js",
-      "@itwin/presentation-components": "@itwin/presentation-components/lib/esm/presentation-components.js",
-    },
-  },
   test: {
     environment: "happy-dom",
+    server: {
+      deps: {
+        inline: ["@itwin/presentation-components", "@itwin/components-react", "@itwin/core-react"],
+      },
+    },
   },
 });
